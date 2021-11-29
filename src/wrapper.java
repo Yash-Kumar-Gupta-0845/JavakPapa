891794c8a5f190b006ba05bdd6dcd1163f024177
Yash-Kumar-Gupta-0845 <yash.gupta0845@gmail.com>
2021-11-30 01:57:34 +0530

    Initial commit

---
+++ src/wrapper.java
@@ -0,0 +1,10 @@
+public class wrapper {
+    public static void main(String[] args) {
+        int i=100;
+        Integer j=Integer.valueOf(i);
+        Integer ab=i;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(ab);
+    }
+}
